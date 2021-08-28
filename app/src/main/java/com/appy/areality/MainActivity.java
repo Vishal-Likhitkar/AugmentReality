47dc173d3ec4668af601807fc44a80c67745fef7
Vishal likhitkar <75991283+Vishal-Likhitkar@users.noreply.github.com>
2021-08-28 23:50:20 +0530

    first commit

---
+++ app/src/main/java/com/appy/areality/MainActivity.java
@@ -0,0 +1,49 @@
+package com.appy.areality;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    Anchor anchor = hitResult.createAnchor();
+                    ModelRenderable.builder()
+                            .setSource(this,Uri.parse("TocoToucan.sfg"))
+                            .build()
+                            .thenAccept(modelRenderable -> addModelToScene(anchor,modelRenderable));
+
+                });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode node = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(modelRenderable);
+        node.setParent(node);
+        arFragment.getArSceneView().getScene().addChild(node);
+        transformableNode.select();
+    }
+}
